fd300993c51af2a33c134e0eef17d7552dd6c103
Juan Navarro <navarrj@shure.com>
2024-05-03 11:46:20 -0600

    log

--- push.js
+++ push.js
@@ -8,6 +8,6 @@ let vapidKeys = {"publicKey":"BF_lG-icS6HUkP_RD2_3Bob3iGw4_d875GJwudFbQ_I-zHfS3m
 
 push.setVapidDetails('mailto:juan.m.navarro@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dCtL-olwwxQ:APA91bFYn3_MnG6qlFmuOb2GxWJ6E1UAiW-WPz4iAnG3Qg_DXE2QH1WFoTPt-IbldPCJQEVgrGhx4QFE58XfR_aPmYwxIV_aSknbi0oMdfyk8xBRiG7l49s7LDF4-4PbOD6f_qm9z2W0","expirationTime":null,"keys":{"p256dh":"BGRA4DboO3G87btmLiDuFM8L2GZ6V7DiNNF3LgpucO9eDR9frPCOYRl22gOy5oq94qmeqSp65yVCOKWr_EcoEXs","auth":"sP79LgkBV9GEDq-QpfknSw"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cdBCyTbg4Hk:APA91bHFKt8s64Y3d6_YLJtAfwG-VRQ7_mF1OGSQBAKbaY5VZk8B92mF-t2w-keJhHwmAcUDZ4xrMAszonERoLYQmgGNR7TJx6BT0AKLHu9BFf5bR9G8fAnbu8LsOM6dTxyKTs1qI_6-","expirationTime":null,"keys":{"p256dh":"BBTGcMQ4foVAfK0pOvdxr4QkNUPem-PG5kSD319bhlOUMUQwn9tbqykFgztQGP05XZDi8KxJ4_y9LsXR9HazPJo","auth":"l8KJRzHxjbBOFvF18jVqkA"}};
 
-push.sendNotification(sub, 'test message');
+push.sendNotification(sub, 'Your device has reached a battery level of 5%.');
--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 self.addEventListener('push', (e) => {
     console.log("received", e);
-    
+    const obj = e.data.json();
+    console.log("obj", obj);
+
     self.registration.showNotification('test message', {});
 });
