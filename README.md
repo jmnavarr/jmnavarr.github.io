b5f9685dde2b644d0f7c5c7892170f5f0c05221b
Juan Navarro <navarrj@shure.com>
2024-05-03 11:05:59 -0600

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+{"endpoint":"https://fcm.googleapis.com/fcm/send/dkwj8sZFzwE:APA91bEJZOhn6xnEcCFXNgemi9V9C_nTwydOcvgB46DcgJ18ULdTU86gNvHeX3wdFsYDsCZC8eCvfKoTdQrwu-sIgi2WQ9MjelqRfHEOJKBRWDI1jv7LS0GCnNoRRr__ENe-gVsDZ_Yc","expirationTime":null,"keys":{"p256dh":"BOX3vEqL-vEOwbE-V5ldqr0Wtgt5zqjLcpNNpFFy8cKb513045ed6ykulE6CfqYIYGSB5PhN4DJRuOLncnJSgwE","auth":"Aqg5pqvqTFfPaHgjn_3Hwg"}}
--- push.js
+++ push.js
@@ -8,4 +8,6 @@ let vapidKeys = {"publicKey":"BF_lG-icS6HUkP_RD2_3Bob3iGw4_d875GJwudFbQ_I-zHfS3m
 
 push.setVapidDetails('mailto:juan.m.navarro@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dkwj8sZFzwE:APA91bEJZOhn6xnEcCFXNgemi9V9C_nTwydOcvgB46DcgJ18ULdTU86gNvHeX3wdFsYDsCZC8eCvfKoTdQrwu-sIgi2WQ9MjelqRfHEOJKBRWDI1jv7LS0GCnNoRRr__ENe-gVsDZ_Yc","expirationTime":null,"keys":{"p256dh":"BOX3vEqL-vEOwbE-V5ldqr0Wtgt5zqjLcpNNpFFy8cKb513045ed6ykulE6CfqYIYGSB5PhN4DJRuOLncnJSgwE","auth":"Aqg5pqvqTFfPaHgjn_3Hwg"}};
+
 push.sendNotification(sub, 'test message');
