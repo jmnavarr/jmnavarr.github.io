b64647a338311637d8025f6e6860b27663ea132b
Juan Navarro <navarrj@shure.com>
2024-05-03 13:27:57 -0600

    log

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Demo Procedure
+
+- Step 1: Go to https://jmnavarr.github.io/
+- Step 2: Click "inspect" and view the browser console
+- Step 3: Click the "Subscribe" button and observe a JSON object that looks like this:
+
+```
 {"endpoint":"https://fcm.googleapis.com/fcm/send/dKzyeGFe5Wo:APA91bFWfqXYY-79r3_P7Nc_IT7g6dUqIG0vWaPFxyUc2cFfYGTBZczEYiANjeG-qkql4zlVz1ba2p4kJd4JvHK7OGuMT-qlL4IlqB4dIzUGr1NMkAXmYeSjcidVE97vy_6RZSjFM9JH","expirationTime":null,"keys":{"p256dh":"BGrrtaGDyFlYHy60HFDQU8QRgSK-DHPmIUrjpr7x5et9XjnanLOqu4ILlWokzNO50ERYCNY0EP4lKCw9aC-iPTs","auth":"TS5EtdpYGkEJ7XpeilTnlA"}}
+```
+
+- Step 4: Update the "sub" variable with the JSON object from above in the `push.js` server-side script
+- Step 5: Run `node push.js` to send a notification to the subscribed client
--- push.js
+++ push.js
@@ -8,7 +8,7 @@ let vapidKeys = {"publicKey":"BF_lG-icS6HUkP_RD2_3Bob3iGw4_d875GJwudFbQ_I-zHfS3m
 
 push.setVapidDetails('mailto:juan.m.navarro@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dX3fg5uznPI:APA91bHmmoSbENhbE0OWsdgHsJbknlogogbVoVkSKMJ5ja9eaM2yI-JQumEY2H1lL22-_UhATzwld6jZIYuKBuICuCZwVgABgIcE5Qrrxtu9Wslv0GGWqdJ-fc2k5U4kpanL6QphVnvb","expirationTime":null,"keys":{"p256dh":"BEEHBHR3iluNiZ6VlNrR6YNQe-cWcaLlDfUqo6FKHgj7au07CLcCDL738kuDvYrrqg1UXaD7QLkgygYv_d1-o88","auth":"JlEGsE3nKdDPkajzctFMoQ"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cyK4nIMGNao:APA91bE7GTJ2anpO-BEBMzmp_af5EqOQ68QCuwh4lnMPJTwHA8IFDT55WyGU_UgAlmsM9ZJ6bD2DoE3GJq1Uv2IAlvk4y88hxr2S7kyps6PiRMzlJU8tJOaVKQks8WyemzMRQocKysmS","expirationTime":null,"keys":{"p256dh":"BMxgP2seOUOb6iKwu0BHExh4TI9yncLzDQIDAO9DjIfWQvvzqPRuLK_qEjPlnZ3IUPFj6yxdEwBaWlzEwMepGHQ","auth":"0-RAGJwm0Yj_jmZ45vta9g"}};
 
 const message = {
     title: "Battery alert",
