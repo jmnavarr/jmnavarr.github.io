31dd9dc1df152ac13b10703857660326b5dca65a
Juan Navarro <navarrj@shure.com>
2024-05-03 11:36:42 -0600

    log

--- push.js
+++ push.js
@@ -8,6 +8,6 @@ let vapidKeys = {"publicKey":"BF_lG-icS6HUkP_RD2_3Bob3iGw4_d875GJwudFbQ_I-zHfS3m
 
 push.setVapidDetails('mailto:juan.m.navarro@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cmcfA2S8wxA:APA91bHM9zLc8bKrUG2wbqU06elydTRmRGV-lQlW0D8gf6-WsD0gOdPsrVqtT-gdZLXV6UGrkq8I_2hoZu2AUhcmWOzMVfjPQm-O0oGJggTqMXgV27GSiL8gzmJpVx1jNM6sB1J4imEq","expirationTime":null,"keys":{"p256dh":"BCM3vNB8aD0D_LPUPbJ1u5zGiVCUFUbHrur7iv0Fk1vuAvP4d4q3oUS4GGEOtJCza0UGzDi33Dgm6ZB70TIvWe8","auth":"hBjXMXHK97nrqM0iOOBICw"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dCtL-olwwxQ:APA91bFYn3_MnG6qlFmuOb2GxWJ6E1UAiW-WPz4iAnG3Qg_DXE2QH1WFoTPt-IbldPCJQEVgrGhx4QFE58XfR_aPmYwxIV_aSknbi0oMdfyk8xBRiG7l49s7LDF4-4PbOD6f_qm9z2W0","expirationTime":null,"keys":{"p256dh":"BGRA4DboO3G87btmLiDuFM8L2GZ6V7DiNNF3LgpucO9eDR9frPCOYRl22gOy5oq94qmeqSp65yVCOKWr_EcoEXs","auth":"sP79LgkBV9GEDq-QpfknSw"}};
 
 push.sendNotification(sub, 'test message');
--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 self.addEventListener('push', (e) => {
     console.log("received", e);
-    e.waitUntil(
-        self.registration.showNotification('test message', {})
-    );
+    
+    self.registration.showNotification('test message', {});
 });
