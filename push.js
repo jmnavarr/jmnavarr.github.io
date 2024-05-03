b04730dd92554b93aeaf1664e71d48faa5f00b23
Juan Navarro <navarrj@shure.com>
2024-05-03 11:53:08 -0600

    log

--- push.js
+++ push.js
@@ -8,11 +8,11 @@ let vapidKeys = {"publicKey":"BF_lG-icS6HUkP_RD2_3Bob3iGw4_d875GJwudFbQ_I-zHfS3m
 
 push.setVapidDetails('mailto:juan.m.navarro@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/djlEpTh3_HI:APA91bFkcxFo1c868JxtxgkGfz86KC1FXlfPnA1ShgQHsUEOYMqpITyYN7600Svri3egid0-kyhf9st24ITuxQMhKHE5u31uS6zCQC_BTl27CYlqPCRzE_AgcgLLif_YawgQ9fCeSbjP","expirationTime":null,"keys":{"p256dh":"BPYACkdsDbSVx3mstgHN8O6xUs63vsJR_NymxBbTxCDR3b09ThTVAVaV2butSA3pCLJWwj_1GIfvL766aXYVWkw","auth":"nUhTngxwCiF3dzwJeyR98g"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dX3fg5uznPI:APA91bHmmoSbENhbE0OWsdgHsJbknlogogbVoVkSKMJ5ja9eaM2yI-JQumEY2H1lL22-_UhATzwld6jZIYuKBuICuCZwVgABgIcE5Qrrxtu9Wslv0GGWqdJ-fc2k5U4kpanL6QphVnvb","expirationTime":null,"keys":{"p256dh":"BEEHBHR3iluNiZ6VlNrR6YNQe-cWcaLlDfUqo6FKHgj7au07CLcCDL738kuDvYrrqg1UXaD7QLkgygYv_d1-o88","auth":"JlEGsE3nKdDPkajzctFMoQ"}};
 
 const message = {
     title: "Battery alert",
-    body: "Your device has reached a battery level of 5%."
+    body: "Battery level < 5%"
 }
 
-push.sendNotification(sub, message);
+push.sendNotification(sub, JSON.stringify(message));
