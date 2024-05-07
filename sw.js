0168be0b6a644dc0ee43fbd89e93de2926463417
Juan Navarro <juan.m.navarro@gmail.com>
2024-05-07 16:02:01 -0500

    add message to dom from sw

--- sw.js
+++ sw.js
@@ -2,6 +2,15 @@ self.addEventListener('install', () => {
     self.skipWaiting();
 });
 
+self.sendMessageToClient = async (message) => {
+    // Send to any window client controlled by this service worker
+    const clientList = await clients.matchAll({ type: "window" });
+    for (let client of clientList) {
+        client.postMessage(message);
+        console.log(`${message} sent to window client`,client)
+    }
+}
+
 self.addEventListener('push', (e) => {
     console.log("received", e);
     const obj = e.data.json();
@@ -17,6 +26,8 @@ self.addEventListener('push', (e) => {
             url: 'https://jmnavarr.github.io/'
         }
     };
+
+    self.sendMessageToClient(obj);
 
     const res = self.registration.showNotification(obj.title, options);
 
